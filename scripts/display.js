e1bc058127a8dca52e2e12a3e3a288d14e23ae60
madyishere <madhavsharma4002@gmail.com>
2023-12-13 14:35:45 +0530

    feat: style changes

--- scripts/display.js
+++ scripts/display.js
@@ -1,18 +1,26 @@
 const detail = document.getElementById("details");
 const img = document.getElementById("picture");
+
 const setbook = () => {
   const current = localStorage.getItem("active");
+
   const getbooks = async () => {
-  const response = await fetch("scripts/books.json");
-  const booklist = await response.json();
-  const updata = () => {
-    console.log(current);
-    console.log(booklist[0].name);
-    let imgform = `
-    <div class="picture"> <img src="${booklist[current].picture}" class="bimg"></div>`
-    img.innerHTML += imgform;
-    let cardformat = `
-      <div class="card">
+    const response = await fetch("scripts/books.json");
+    const booklist = await response.json();
+
+    const updata = () => {
+      console.log(current);
+      console.log(booklist[0].name);
+
+      // Make image smaller and responsive
+      let imgform = `
+        <div class="picture">
+          <img src="${booklist[current].picture}" class="bimg img-fluid" width="500px" alt="Book Image">
+        </div>`;
+      img.innerHTML += imgform;
+
+      let cardformat = `
+        <div class="">
           <strong><center><h1>${booklist[current].name}</h1></center></strong>
           <strong><p class="head">Author:</strong> ${booklist[current].author}<br>
           <strong>Edition:</strong> ${booklist[current].edition}<br>
@@ -20,16 +28,12 @@ const setbook = () => {
           <strong>Posted By:</strong> ${booklist[current].by}<br>
           <strong>Contact:</strong> ${booklist[current].contact}</p><br>
           <p></p>
-      </div>`;
+        </div>`;
       detail.innerHTML += cardformat;
-      };
+    };
+
     updata();
+  }
+
+  getbooks();
 }
-getbooks();
-}
-//SEARCHING FUNCTIONALITY
-const query = document.querySelector("#searching");
-query.addEventListener('submit', e => {
-  e.preventDefault();
-  console.log(query.value);
-})
